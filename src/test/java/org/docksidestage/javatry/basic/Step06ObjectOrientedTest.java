1c92f962453ac0c91ae9be7ef871df85c4fe9dc0
MasakiKamachi <masaki@kamachimasakiki-no-MacBook-Pro.local>
2019-10-01 23:16:32 +0900

    create step06 test

--- src/test/java/org/docksidestage/javatry/basic/Step06ObjectOrientedTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step06ObjectOrientedTest.java
@@ -371,6 +371,12 @@ public class Step06ObjectOrientedTest extends PlainTestCase {
      */
     public void test_objectOriented_writing_withDelegation() {
         // your confirmation code here
+        Dog dog = new Dog();
+        log(dog.bark());
+        Cat cat = new Cat();
+        log(cat.bark());
+        Zombie zombie = new Zombie();
+        log(zombie.bark());
     }
 
     /**
